51642389ff63a8b799b8b3661dca59359caaeb3a
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-11 14:01:28 -0400

    passed tests using the filter function)

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+// function findMatching(collection, name) {
+//   const newCollection = [];
+//   for (const drivers of collection) {
+//     if (drivers.toLowerCase() === name.toLowerCase()) {
+//       newCollection.push(drivers);
+//     }
+//   }
+// }
+
+
+function findMatching(collection, driver){
+    return collection.filter(name => name.toLowerCase() === driver.toLowerCase());
+}
+
+function fuzzyMatch(collection, driverLetter){
+    return collection.filter(firstLetter => firstLetter[0] === driverLetter[0]);
+}
+
+function matchName(collection, driverObj){
+    return collection.filter(driver => driver.name === driverObj);
+}
